a78678b350ef5ea4ba5830b38a944a3822ff753f
kalatin <pitsik228@mail.ru>
2022-11-27 21:17:21 +0300

    add tabs script and styles

---
+++ js/tabs.js
@@ -0,0 +1,175 @@
+let tabsCode = {
+	tab1:`<div class="event__row">
+	<div class="event__indicator"><img src="icons/event-circle.svg" alt="circle"> 8:00 AM</div>
+	<div class="event__block">
+		<div class="event__block-info">
+			<div class="event__name">Entry Registration & Continental Breakfast</div>
+			<div class="event__where">
+				<div class="event__time"><img src="icons/event-time.svg" alt="tiem">08:00 AM - 09:00 AM</div>
+				<div class="event__location"><img src="icons/event-location.svg" alt="location">Dining Hall</div>
+			</div>
+			<button class="event__view">View Details</button>
+		</div>
+		<div class="event__block-images">
+			<img src="image/Ellipse 1.png" alt="">
+		</div>
+	</div>
+</div>
+<div class="event__row ">
+	<div class="event__indicator "><img src="icons/event-circle.svg" alt="circle"> 9:00 AM</div>
+	<div class="event__block ">
+		<div class="event__block-info">
+			<div class="event__name">Introduction Creative Design | Carrier Guideline</div>
+			<div class="event__where">
+				<div class="event__time"><img src="icons/event-time.svg" alt="tiem">09:00 AM - 10:00 AM</div>
+				<div class="event__location"><img src="icons/event-location.svg" alt="location">Hall of Fame 01</div>
+			</div>
+			<button class="event__view">View Details</button>
+		</div>
+		<div class="event__block-images">
+			<img src="image/Ellipse 2.png" alt="1">
+			<img src="image/Ellipse 3.png" alt="1">
+			<img src="image/Ellipse 4.png" alt="1">
+			<img src="image/Ellipse 5.png" alt="1">
+		</div>
+	</div>
+</div>
+<div class="event__row">
+	<div class="event__indicator"><img src="icons/event-circle.svg" alt="circle"> 10:00 AM</div>
+	<div class="event__block">
+		<div class="event__block-info">
+			<div class="event__name">Introduction UX Design | Carrier Guideline</div>
+			<div class="event__where">
+				<div class="event__time"><img src="icons/event-time.svg" alt="tiem">10:00 AM - 12:00 AM</div>
+				<div class="event__location"><img src="icons/event-location.svg" alt="location">Hall of Fame 01</div>
+			</div>
+			<button class="event__view">View Details</button>
+		</div>
+		<div class="event__block-images">
+			<img src="image/Ellipse 2.png" alt="1">
+			<img src="image/Ellipse 3.png" alt="1">
+			<img src="image/Ellipse 4.png" alt="1">
+			<img src="image/Ellipse 5.png" alt="1">
+		</div>
+	</div>
+</div>
+<div class="event__row">
+	<div class="event__indicator"><img src="icons/event-circle.svg" alt="circle"> 12:00 AM</div>
+	<div class="event__block">
+		<div class="event__block-info">
+			<div class="event__name">Lunch Break</div>
+			<div class="event__where">
+				<div class="event__time"><img src="icons/event-time.svg" alt="tiem">10:00 AM - 02:00 AM</div>
+				<div class="event__location"><img src="icons/event-location.svg" alt="location">Dining Hall</div>
+			</div>
+			<button class="event__view">View Details</button>
+		</div>
+		<div class="event__block-images">
+			<img src="image/Ellipse 1.png" alt="1">
+		</div>
+	</div>
+</div>`,
+	tab2:`<div class="event__row">
+	<div class="event__indicator"><img src="icons/event-circle.svg" alt="circle"> 10:00 AM</div>
+	<div class="event__block">
+		<div class="event__block-info">
+			<div class="event__name">Entry Registration & Continental Breakfast</div>
+			<div class="event__where">
+				<div class="event__time"><img src="icons/event-time.svg" alt="tiem">08:00 AM - 09:00 AM</div>
+				<div class="event__location"><img src="icons/event-location.svg" alt="location">Dining Hall</div>
+			</div>
+			<button class="event__view">View Details</button>
+		</div>
+		<div class="event__block-images">
+			<img src="image/Ellipse 1.png" alt="">
+		</div>
+	</div>
+</div>
+<div class="event__row">
+	<div class="event__indicator"><img src="icons/event-circle.svg" alt="circle"> 12:00 AM</div>
+	<div class="event__block">
+		<div class="event__block-info">
+			<div class="event__name">Introduction UX Design | Carrier Guideline</div>
+			<div class="event__where">
+				<div class="event__time"><img src="icons/event-time.svg" alt="tiem">10:00 AM - 12:00 AM</div>
+				<div class="event__location"><img src="icons/event-location.svg" alt="location">Hall of Fame 01</div>
+			</div>
+			<button class="event__view">View Details</button>
+		</div>
+		<div class="event__block-images">
+			<img src="image/Ellipse 2.png" alt="1">
+			<img src="image/Ellipse 5.png" alt="1">
+		</div>
+	</div>
+</div>
+`,
+	tab3:`
+<div class="event__row">
+	<div class="event__indicator"><img src="icons/event-circle.svg" alt="circle"> 10:00 AM</div>
+	<div class="event__block">
+		<div class="event__block-info">
+			<div class="event__name">Introduction UX Design | Carrier Guideline</div>
+			<div class="event__where">
+				<div class="event__time"><img src="icons/event-time.svg" alt="tiem">10:00 AM - 12:00 AM</div>
+				<div class="event__location"><img src="icons/event-location.svg" alt="location">Hall of Fame 01</div>
+			</div>
+			<button class="event__view">View Details</button>
+		</div>
+		<div class="event__block-images">
+			<img src="image/Ellipse 2.png" alt="1">
+		</div>
+	</div>
+</div>
+<div class="event__row">
+	<div class="event__indicator"><img src="icons/event-circle.svg" alt="circle"> 11:00 AM</div>
+	<div class="event__block">
+		<div class="event__block-info">
+			<div class="event__name">Introduction UX Design | Carrier Guideline</div>
+			<div class="event__where">
+				<div class="event__time"><img src="icons/event-time.svg" alt="tiem">10:00 AM - 12:00 AM</div>
+				<div class="event__location"><img src="icons/event-location.svg" alt="location">Hall of Fame 01</div>
+			</div>
+			<button class="event__view">View Details</button>
+		</div>
+		<div class="event__block-images">
+			<img src="image/Ellipse 4.png" alt="1">
+		</div>
+	</div>
+</div>
+<div class="event__row">
+	<div class="event__indicator"><img src="icons/event-circle.svg" alt="circle"> 12:00 AM</div>
+	<div class="event__block">
+		<div class="event__block-info">
+			<div class="event__name">Introduction UX Design | Carrier Guideline</div>
+			<div class="event__where">
+				<div class="event__time"><img src="icons/event-time.svg" alt="tiem">10:00 AM - 12:00 AM</div>
+				<div class="event__location"><img src="icons/event-location.svg" alt="location">Hall of Fame 01</div>
+			</div>
+			<button class="event__view">View Details</button>
+		</div>
+		<div class="event__block-images">
+			<img src="image/Ellipse 2.png" alt="1">
+			<img src="image/Ellipse 3.png" alt="1">
+			<img src="image/Ellipse 5.png" alt="1">
+		</div>
+	</div>
+</div>`
+};
+
+
+
+let tabsButtons = document.querySelectorAll('.event__button');
+
+
+tabsButtons.forEach(item => {
+	item.addEventListener('click',() => {
+		if(item.closest('active-tab')) {
+			console.log(23);
+		}else {
+			tabsButtons.forEach(item => {item.classList.remove('active-tab')});
+			item.classList.add('active-tab')
+		}
+		let tabHTML = tabsCode[item.dataset.target];
+		document.querySelector('.event__area').innerHTML = tabHTML;
+	})
+})
